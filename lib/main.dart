88fac7af2d29502ab0009b49feb0c8df3a6aa25a
roflixd <roflixd25@gmail.com>
2023-10-04 03:06:45 +0300

    update TODO

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:me_restaurant/pages/login.dart';
 import 'package:me_restaurant/pages/main.dart';
 import 'package:me_restaurant/pages/register.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 /*  TODO:
 
 # CRITICAL
  
-
 # MOST IMPORTANT
- 1) add Sqlite to this?
- 2) store userr's info in db
- 3) enctypt password to store it
- 4) store token model in cache
- 5) on startup check if user siggned(if refresh tokent is valid)
+ 1) on startup check if user siggned(if refresh tokent is valid)
+    -> Splash screen will help with it
         -> if not go to login screen
-
+ 
 # LESS IMPORTANT
   1) Add 'hint' to login and register pages
+
 */
 
 void main() {  
